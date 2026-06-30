32436d5957db33b96f2e5318a39d33688af97218
david <dadsad951@gmail.com>
2026-07-01 02:46:56 +0300

    43.3

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,15 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "build"
-  },
   server: {
-    host: "0.0.0.0",
-    port: 3000
+    port: 3000,
   },
   preview: {
-    host: "0.0.0.0",
-    port: 3000
-  }
+    port: 3000,
+  },
+  build: {
+    outDir: "build",
+  },
 });
