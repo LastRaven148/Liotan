62dec5a15488ebdfbccb835e509aba9c59db6322
david <dadsad951@gmail.com>
2026-07-05 16:32:56 +0300

    fix ru api endpoint in client config

--- client/src/config/api.jsx
+++ client/src/config/api.jsx
@@ -1,4 +1,4 @@
-const PRODUCTION_API_URL = "https://api.liotan.com";
+const PRODUCTION_API_URL = "https://api.liotan.ru";
 const DEVELOPMENT_API_URL = "http://localhost:3001";
 
 function normalizeApiUrl(url) {
@@ -23,7 +23,7 @@ function resolvePrimaryApiUrl() {
 
 export const API = resolvePrimaryApiUrl();
 export const API_CANDIDATES = [API];
-export const PRODUCTION_CLIENT_URL = "https://liotan.com";
+export const PRODUCTION_CLIENT_URL = "https://liotan.ru";
 export const PRODUCTION_API_URL_VALUE = PRODUCTION_API_URL;
 
 export function getApiCandidates() {
